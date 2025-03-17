166e63573a995b26a47dd20c5912b3e6e1d1c219
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-03-17 10:35:57 +0530

    get input value task done

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,9 @@
 function changeColor() {
     document.getElementById("para").style.color = "blue";
 }
+
+// get input value
+function getValueBtn() {
+    const inputValue = document.getElementById("input-box").value;
+    alert(inputValue);
+}
